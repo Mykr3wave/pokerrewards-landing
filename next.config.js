1683431c823998d9b8fac81d73eb1011cedb3e84
Mykr3wave <ivarorn98@gmail.com>
2025-06-05 20:34:36 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,38 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // No need to configure "images.domains" for local public/ files.
-  // If you only use <img src="/foo.png" />, you don't need anything here.
-  images: {
-    // You can remove this entire `images` section if you're only using local assets.
-    // But if you want to keep it, it must be at the root level, NOT under pages/.
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cryptoleakv3.vercel.app",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "coinpoker.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ggpoker.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.unibet.eu",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.bet365.com",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
