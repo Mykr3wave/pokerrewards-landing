7ed524a709a044d2ec3a5bb91156a0c1ea12d618
Mykr3wave <ivarorn98@gmail.com>
2025-06-05 06:47:44 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -82,3 +82,10 @@ export default function Home() {
     </main>
   );
 }
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-8 bg-black text-white">
+      {/* ...content... */}
+    </main>
+  );
+}  <--- Correct
