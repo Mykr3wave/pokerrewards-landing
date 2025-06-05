815b924ff4863352cadf1520e6bb66189b841614
Mykr3wave <ivarorn98@gmail.com>
2025-06-05 21:56:33 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * next.config.js
- *
- * We are only loading local images from /public/images/, so no extra `images.domains` config is required.
- */
-module.exports = {
-  reactStrictMode: true
-};
