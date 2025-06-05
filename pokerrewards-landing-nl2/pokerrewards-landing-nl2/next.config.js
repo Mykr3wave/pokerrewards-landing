55a55aebb141b82ad8cd04156d54f80da2ac2d39
Mykr3wave <ivarorn98@gmail.com>
2025-06-05 22:34:09 +0200

    Add files via upload

---
+++ pokerrewards-landing-nl2/pokerrewards-landing-nl2/next.config.js
@@ -0,0 +1,9 @@
+/**
+ * next.config.js
+ *
+ * We are loading only local images from `public/images/`, so no extra
+ * `images.domains` configuration is needed.
+ */
+module.exports = {
+  reactStrictMode: true
+};
