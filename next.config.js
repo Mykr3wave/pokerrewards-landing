3640b029e2a660c5104332cdb887caf548ae628d
Mykr3wave <ivarorn98@gmail.com>
2025-06-05 07:15:10 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,29 +3,24 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cryptoleakv3.vercel.app",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'cryptoleakv3.vercel.app',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "coinpoker.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'coinpoker.com',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "ggpoker.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'ggpoker.com',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "www.unibet.eu",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.bet365.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'www.unibet.eu',
+        pathname: '/**',
       },
     ],
   },
