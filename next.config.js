6bb5b4e58f57938df920ef0812283e10c1664d1e
Mykr3wave <ivarorn98@gmail.com>
2025-06-05 20:58:20 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // All images are served from /public, so this can remain empty.
+    // If you later need external domains, add them under remotePatterns.
+    remotePatterns: []
+  }
+};
 
 module.exports = nextConfig;
