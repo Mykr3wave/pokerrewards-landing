3dd07d17e27840bf8f046d424b20ff4e7c834b18
Mykr3wave <ivarorn98@gmail.com>
2025-06-07 10:32:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,62 +6,14 @@ import Link from 'next/link'
 
 export default function Home() {
   const rooms = [
-    {
-      name: 'TigerGaming',
-      logo: '/images/tigergaming.png',
-      href: 'https://tigergaming.nl',
-      bonus: '100% up to $1,000',
-      rakeback: 'VIP'
-    },
-    {
-      name: 'WPT Global',
-      logo: '/images/wptglobal.png',
-      href: 'https://wptglobal.com',
-      bonus: '100% up to $3,000',
-      rakeback: 'VIP'
-    },
-    {
-      name: 'CoinPoker',
-      logo: '/images/coinpoker.png',
-      href: 'https://coinpoker.nl',
-      bonus: '150% up to $2,000',
-      rakeback: '33%+'
-    },
-    {
-      name: '4Poker',
-      logo: '/images/4poker.png',
-      href: 'https://4poker.com',
-      bonus: '400% up to $1,000',
-      rakeback: 'Up to 50%'
-    },
-    {
-      name: 'Phenom Poker',
-      logo: '/images/phenompoker.png',
-      href: 'https://play.phenompoker.com/register?r=2I3JPR',
-      bonus: '–',
-      rakeback: '–'
-    },
-    {
-      name: 'iPoker.it Italy',
-      logo: '/images/ipokerit.png',
-      href: 'https://ipoker.it',
-      bonus: '–',
-      rakeback: '–'
-    },
-    {
-      name: 'GGPoker',
-      logo: '/images/ggpoker.png',
-      href: 'https://ggpoker.com',
-      bonus: '–',
-      rakeback: '–'
-    },
-    {
-      name: 'Unibet',
-      logo: '/images/unibet.png',
-      href: 'https://unibet.nl',
-      bonus: '–',
-      rakeback: '–'
-    }
+    { name: 'TigerGaming', logo: '/images/tigergaming.png', href: 'https://tigergaming.nl', bonus: '100% up to $1,000', rakeback: 'VIP' },
+    { name: 'WPT Global', logo: '/images/wptglobal.png', href: 'https://wptglobal.com', bonus: '100% up to $3,000', rakeback: 'VIP' },
+    { name: 'CoinPoker', logo: '/images/coinpoker.png', href: 'https://coinpoker.nl', bonus: '150% up to $2,000', rakeback: '33%+' },
+    { name: '4Poker', logo: '/images/4poker.png', href: 'https://4poker.com', bonus: '400% up to $1,000', rakeback: 'Up to 50%' },
+    { name: 'Phenom Poker', logo: '/images/phenompoker.png', href: 'https://play.phenompoker.com/register?r=2I3JPR', bonus: '–', rakeback: '–' },
+    { name: 'iPoker.it Italy', logo: '/images/ipokerit.png', href: 'https://ipoker.it', bonus: '–', rakeback: '–' },
+    { name: 'GGPoker', logo: '/images/ggpoker.png', href: 'https://ggpoker.com', bonus: '–', rakeback: '–' },
+    { name: 'Unibet', logo: '/images/unibet.png', href: 'https://unibet.nl', bonus: '–', rakeback: '–' }
   ]
 
   return (
